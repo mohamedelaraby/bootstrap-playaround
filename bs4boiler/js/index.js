1773f903a50ebce84b91b6a522e42b9d3d8ead02
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-10-18 18:31:42 +0200

    Splexa project structure

---
+++ bs4boiler/js/index.js
@@ -0,0 +1,9 @@
+/*
+[  author ] ;- Muhammed alaraby
+[  Date   ] ;- 19/10/19
+[  Info   ] ;- Main script file
+*/
+
+//Get the current year for the copyright
+
+$('#year').text(new Date().getFullYear());
